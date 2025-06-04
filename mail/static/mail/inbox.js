30c743f7382b4f878b30c396ec3ec66800c95d5d
Odai Gubran <odaimansour777@gmail.com>
2025-06-04 22:58:01 +0300

    added Mailbox: When a user visits their Inbox, Sent mailbox, or Archive, load the appropriate mailbox.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -29,9 +29,9 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log(result);
       });
 
-      recpitients.value = '';
-      subject.value = '';
-      body.value = '';
+    recpitients.value = '';
+    subject.value = '';
+    body.value = '';
     return false;
   }
 
@@ -50,6 +50,8 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
+  // Hide the Full email by removing the #mail-container innerHTML
+  document.querySelector("#mail-container").innerHTML = '';
 
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
@@ -57,6 +59,66 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-  
-  
+
+  // Fetch all emails
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+      for (let email of emails) {
+        console.log(email)
+        const mailDiv = document.createElement("div");
+        mailDiv.className = "mail-div";
+        mailDiv.innerHTML = `
+            <strong class="mail-sender">${email.sender}</strong>
+            <span class="mail-subject">${email.subject}</span>
+            <span class="mail-timestamp">${email.timestamp}</span>
+        `;
+        document.querySelector("#emails-view").append(mailDiv);
+
+        // Change the mailDiv coloring style based on read status
+        if (email.read === true) {
+          mailDiv.classList.toggle("read");
+        } else {
+          mailDiv.classList.toggle("unread")
+        }
+
+        // Open the full email when it's div is clicked
+        mailDiv.addEventListener('click', () => {
+          document.querySelector("#emails-view").style.display = 'none';
+
+          const fullMail = document.createElement("div");
+          fullMail.classList.add("full-mail")
+
+          fetch(`/emails/${email.id}`)
+            .then(response => response.json())
+            .then(email => {
+              console.log(email)
+              fullMail.innerHTML = `<button class="back-btn btn btn-sm btn-outline-primary">Back</button>
+                                <p><strong>From: </strong> ${email.sender}</p>
+                                <p><strong>To: </strong> ${email.recipients}</p>
+                                <p><strong>Subject: </strong>${email.subject}</p>
+                                <button class="btn btn-sm btn-outline-primary">Reply</button>
+                                <hr>
+                                ${email.subject}`
+              document.querySelector("#mail-container").append(fullMail)
+
+              // Back button to recursively call the load_mailbox function 
+              const backBtn = document.querySelector(".back-btn").addEventListener('click', () => {
+              load_mailbox(mailbox);
+          })
+            })
+          
+          // Set the email read value to true 
+          fetch(`/emails/${email.id}`, {
+            method : 'PUT',
+            body: JSON.stringify({
+              read: true
+            })
+          })
+          
+          
+            
+        });
+      }
+    });
 }
